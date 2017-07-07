3bf10352d20d8978b0aeeb0b9bb914d655c090ed
iiiuuu <xulong940331@163.com>
2017-07-07 17:35:53 +0800

    setting中去掉爬虫等待时间的显示，ajk中改为按照分页爬去，由于爬取首页跟分页会出现递归爬取的情况，因此改为从一页一直到最后一页改为分页爬取，这样就不会出现递归爬取的情况，但是现在只爬出658条数据而且存在重复数据，下一步需要 1.去除重复数据， 2.分析其他未爬取数据的原因

--- AJK/AJK/spiders/AJK.py
+++ AJK/AJK/spiders/AJK.py
@@ -12,6 +12,30 @@ class AJK(scrapy.Spider):
 
     def parse(self, response):
         soup = BeautifulSoup(response.body, 'html.parser')
+        #数据信息列
+        infos = soup.findAll(attrs={'class': 'item-mod'})
+        # 分页部分
+        pagesUrl = soup.find(attrs={'class': 'list-page'})
+        print("开始计算页码个数")
+        # 楼盘总数
+        number = int(pagesUrl.find(attrs={'class': 'total'}).em.string)
+        # 页码总数其中每页固定50个数据
+        pages = number // 50
+        if (number % 50 > 0):
+            pages = pages + 1
+        print("一共" + str(pages))
+        purl = pagesUrl.find(attrs={'class': 'pagination'}).a['href']
+        purl = purl[0:-3]
+        for i in range(1, pages + 1):
+            temp = purl + "p" + str(i) + "/"
+            print("生成需要爬去的页面链接" + temp)
+            print("发送请求" + temp)
+            yield scrapy.Request(temp, callback=self.parse_item)
+            print("处理数据完毕")
+
+    def parse_item(self, response):
+        soup = BeautifulSoup(response.body, 'html.parser')
+        # 数据信息列
         infos = soup.findAll(attrs={'class': 'item-mod'})
         for q in infos:
             if 'data-link' in str(q):
@@ -26,12 +50,11 @@ class AJK(scrapy.Spider):
                     item['price'] = q.find(attrs={'class': 'price'}).span.string
                     print(q.find(attrs={'class': 'price'}).span.string)
                 else:
-                    item['price'] = q.find(attrs={'class': 'favor-tag around-price'}).span.string+'around'
-                    print(q.find(attrs={'class': 'favor-tag around-price'}).span.string+'around')
-                item['telephone'] = q.find(attrs={'class': 'tel'}).string
+                    item['price'] = q.find(attrs={'class': 'favor-tag around-price'}).span.string + 'around'
+                    print(q.find(attrs={'class': 'favor-tag around-price'}).span.string + 'around')
+                item['telephone'] = q.find(attrs={'class': 'tel'}).contents[1]
                 print(q.find(attrs={'class': 'tel'}).string)
                 yield item
-
 
 
 
